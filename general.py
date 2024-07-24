b886869c7087c5a8c019186579aca98cc5ffe16a
maxkigo <maximiliano.lopez@cargomovil.com>
2024-07-23 18:41:55 -0600

    despliegue

--- general.py
+++ general.py
@@ -3,14 +3,11 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from google.cloud import bigquery
-from datetime import datetime, timedelta
+from datetime import datetime
 import plotly.express as px
 from plotly.subplots import make_subplots
-import base64
-import os
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import requests
+import json
 from google.oauth2 import service_account
 
 st.set_page_config(layout="wide")
@@ -35,26 +32,25 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 client = bigquery.Client(credentials=credentials)
 
-# Datos de acceso a Gmail
-MY_EMAIL = st.secrets["mail"]["MY_EMAIL"]
-MY_PASSWORD = st.secrets["mail"]["MY_PASSWORD"]
+# Configuración del bot de Telegram
+TELEGRAM_BOT_TOKEN = st.secrets["telegram"]["BOT_TOKEN"]
+TELEGRAM_CHAT_ID = st.secrets["telegram"]["CHAT_ID"]
 
-
-# Función para enviar correo electrónico
-def enviar_alerta_email(subject, body):
-    msg = MIMEMultipart()
-    msg['From'] = MY_EMAIL
-    msg['To'] = 'mxlopez8@gmail.com'
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-
-    server = smtplib.SMTP('smtp.gmail.com: 587')
-    server.starttls()
-    server.login(msg['From'], MY_PASSWORD)
-    server.sendmail(msg['From'], msg['To'], msg.as_string())
-    server.quit()
-    print("Correo de alerta enviado.")
-
+# Función para enviar mensaje a Telegram
+def enviar_alerta_telegram(texto):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = json.dumps({
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": texto
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(url, headers=headers, data=payload)
+    if response.status_code == 200:
+        print("Mensaje de alerta enviado a Telegram.")
+    else:
+        print(f"Error al enviar el mensaje a Telegram: {response.status_code} - {response.text}")
 
 @st.cache_data(ttl=3600)  # Cache data for one hour
 def errores_diario_todos():
@@ -82,12 +78,11 @@ def errores_diario_todos():
     df_errores_diario_todos = client.query(query_errores_diario).to_dataframe()
     return df_errores_diario_todos
 
-
 # Singleton to track if an alert was sent in the current hour
-@st.cache_resource
 def get_alert_sent_status():
-    return {'sent': False, 'timestamp': None}
-
+    if 'alert_status' not in st.session_state:
+        st.session_state.alert_status = {'sent': False, 'timestamp': None}
+    return st.session_state.alert_status
 
 alert_status = get_alert_sent_status()
 current_time = datetime.now()
@@ -107,8 +102,8 @@ if not proyectos_con_errores_altos.empty:
         for idx, row in proyectos_con_errores_altos.iterrows():
             mensaje_alerta += f"Proyecto: {row['proyecto']}, Porcentaje de Error: {row['porcentaje_error']}%\n"
 
-        # Enviar el correo de alerta
-        enviar_alerta_email("Alerta de Errores en Proyectos", mensaje_alerta)
+        # Enviar el mensaje de alerta a Telegram
+        enviar_alerta_telegram(mensaje_alerta)
 
         # Update alert status
         alert_status['sent'] = True
